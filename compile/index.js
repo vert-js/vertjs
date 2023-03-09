eb9cd6142676cbb656ee09d31280abf2c5c84045
Sylvain Gougouzian <sylvain.gougouzian@gmail.com>
2023-03-09 22:12:33 +0100

    fix(compile): :truck: organization

--- compile/index.js
+++ compile/index.js
@@ -1,7 +1,9 @@
 /* eslint-disable no-undef */
 import { mkdirSync, existsSync, rmSync } from "fs";
-import copyStatic from "./copyStatic";
-import transformSrc from "./tranformSrc";
+import calcFinalSize from "./steps/calcFinalSize";
+import copyStatic from "./steps/copyStatic";
+import transformSrc from "./steps/tranformSrc";
+import humanFileSize from "./utils/human";
 
 globalThis.dirs = {
   dist: Bun.env.DIST || `dist`,
@@ -13,5 +15,22 @@ if (existsSync(globalThis.dirs.dist))
   rmSync(globalThis.dirs.dist, { recursive: true, force: true });
 mkdirSync(globalThis.dirs.dist);
 
+globalThis.sizes = {
+  origin: 0,
+  final: 0,
+};
+
+let time = performance.now();
+
 copyStatic();
-transformSrc();
+await transformSrc();
+calcFinalSize();
+
+time = performance.now() - time;
+
+// eslint-disable-next-line no-console
+console.log(
+  `🌱 Original size : ${humanFileSize(globalThis.sizes.origin)}
+   Final size : ${humanFileSize(globalThis.sizes.final)}
+   in ${time.toFixed(3)} ms`
+);
---
+++ compile/steps/tranformSrc.js
@@ -0,0 +1,57 @@
+/* eslint-disable no-console */
+/* eslint-disable no-undef */
+import { readdirSync } from "fs";
+import cssMinifier from "../minifier/css";
+import htmlMinifier from "../minifier/html";
+import humanFileSize from "../utils/human";
+
+export default function transformSrc() {
+  return new Promise((resolve, reject) => {
+    try {
+      const files = readdirSync(globalThis.dirs.src);
+      const promises = [];
+      files.forEach((file) => {
+        promises.push(
+          new Promise((resolveEach) => {
+            const extension = file.split(".").pop();
+            const srcFile = Bun.file(`./${globalThis.dirs.src}/${file}`);
+            const originSize = srcFile.size;
+            globalThis.sizes.origin += originSize;
+            srcFile.text().then((text) => {
+              let content = text;
+              let time = performance.now();
+              switch (extension) {
+                case "html":
+                  content = htmlMinifier(content);
+                  break;
+                case "css":
+                  content = cssMinifier(content);
+                  break;
+                default:
+                  break;
+              }
+              const destFile = Bun.file(`./${globalThis.dirs.dist}/${file}`);
+              Bun.write(destFile, content).then(() => {
+                time = performance.now() - time;
+                resolveEach(
+                  `🗜️  ${file} : from ${humanFileSize(
+                    originSize
+                  )} to ${humanFileSize(destFile.size)} in ${time.toFixed(
+                    3
+                  )} ms`
+                );
+              });
+            });
+          })
+        );
+      });
+      Promise.all(promises).then((logs) => {
+        logs.forEach((log) => console.log(log));
+        resolve();
+      });
+    } catch (e) {
+      console.log(e);
+      reject();
+    }
+  });
+}
