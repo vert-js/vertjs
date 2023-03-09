58dd4b1563da339e0c568ced5b6d34bddee57539
Sylvain Gougouzian <sylvain.gougouzian@gmail.com>
2023-03-09 21:03:45 +0100

    feat(compile): :sparkles: stats

--- compile/tranformSrc.js
+++ compile/tranformSrc.js
@@ -4,12 +4,35 @@ import { readdirSync } from "fs";
 import cssMinifier from "./minifier/css";
 import htmlMinifier from "./minifier/html";
 
+const humanFileSize = (bytes) => {
+  const thresh = 1024;
+  let res = bytes;
+
+  if (Math.abs(bytes) < thresh) {
+    return `${bytes} B`;
+  }
+
+  const units = ["kB", "MB"];
+  let u = -1;
+  const r = 10 ** 3;
+
+  do {
+    res /= thresh;
+    u += 1;
+  } while (Math.round(Math.abs(res) * r) / r >= thresh && u < units.length - 1);
+
+  return `${res.toFixed(3)} ${units[u]}`;
+};
+
 export default function transformSrc() {
   try {
     const files = readdirSync("src");
     files.forEach(async (file) => {
       const extension = file.split(".").pop();
-      let content = await Bun.file(`./src/${file}`).text();
+      const srcFile = Bun.file(`./src/${file}`);
+      const originSize = srcFile.size;
+      let content = await srcFile.text();
+      let time = performance.now();
       switch (extension) {
         case "html":
           content = await htmlMinifier(content);
@@ -20,8 +43,14 @@ export default function transformSrc() {
         default:
           break;
       }
-      await Bun.write(`./dist/${file}`, content);
-      console.log(`🗜️  ${file} done`);
+      const destFile = Bun.file(`./dist/${file}`);
+      await Bun.write(destFile, content);
+      time = performance.now() - time;
+      console.log(
+        `🗜️ ${file} : from ${humanFileSize(originSize)} to ${humanFileSize(
+          destFile.size
+        )} in ${time.toFixed(3)} ms`
+      );
     });
   } catch (e) {
     console.log(e);
