0212576986aa577e6d2153733a937efe50cbfbb3
Sylvain Gougouzian <sylvain.gougouzian@gmail.com>
2023-03-15 22:04:22 +0100

    feat(server): :sparkles: add "router"

--- server/Server.js
+++ server/Server.js
@@ -1,20 +1,24 @@
-"bun run --hot serve.js";
-
+/* eslint-disable no-undef */
 export default class Server {
   static start() {
     // eslint-disable-next-line no-undef
     globalThis.server = Bun.serve({
-      // eslint-disable-next-line no-undef
+      development: Bun.env.development || false,
+      hostname: Bun.env.hostname || "localhost",
       port: Bun.env.PORT || 1337,
-      fetch() {
-        // eslint-disable-next-line no-undef
-        return new Response(Bun.file("./dist/index.html"));
+      fetch(req) {
+        const url = new URL(req.url);
+        if (url.pathname === "/")
+          return new Response(Bun.file("./dist/index.html"));
+        const file = Bun.file(`./dist${url.pathname}`);
+        if (file.size !== 0) return new Response(file);
+        return new Response(`404!`);
       },
     });
     // eslint-disable-next-line no-console
     console.log(
       "🌱 is listening on \x1b[32m%s\x1b[0m",
-      `http://localhost:${globalThis.server.port}`
+      `http://${globalThis.server.hostname}:${globalThis.server.port}`
     );
   }
 }
